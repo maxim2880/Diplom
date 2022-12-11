3b106afbeb644da6d4ab587b01ae7596336c3b1d
admin <misuramaxim@gmail.com>
2022-12-11 18:52:57 +0500

    OAuth2.0

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    path('core/', include('core.urls')),
     path('oauth/', include("social_django.urls", namespace="social")),
 ]
