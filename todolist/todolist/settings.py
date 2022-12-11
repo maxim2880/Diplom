007779551804f8f5e4b3dfbd23883cb96d753de0
admin <misuramaxim@gmail.com>
2022-12-12 02:13:08 +0500

    Debug added default value

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(ENV_FILE_PATH)
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.str('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ["*"]
 
