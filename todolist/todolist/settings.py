7c73d1d5de34c7dfdaea4b4ffb3670475a8a572d
admin <misuramaxim@gmail.com>
2022-12-11 19:14:58 +0500

    OAuth2.0

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'USER': env.str('POSTGRES_USER'),
         'PASSWORD': env.str('POSTGRES_PASSWORD'),
         'HOST': env.str('POSTGRES_HOST', default='127.0.0.1'),
-        'PORT': env.str('POSTGRES_PORT')
+        'PORT': '5432',
     }
 }
 
